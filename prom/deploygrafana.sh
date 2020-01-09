de0a0436fefcad27bf55f8a7a9c51a592656a0b5
陈孟迪 <chenmengdi@chenmengdis-MacBook-Pro.local>
2020-01-09 14:35:17 +0800

    save

--- prom/deploygrafana.sh
+++ prom/deploygrafana.sh
@@ -2,3 +2,9 @@
 kubectl apply -f grafana-dashboard-provider.yaml
 kubectl apply -f grafana.yaml
 kubectl apply -f grafana-service.yaml
+
+
+kubectl delete -f grafana-dashboard-provider.yaml
+kubectl delete -f grafana.yaml
+kubectl delete -f grafana-service.yaml
+

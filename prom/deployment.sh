b0b6e14979607af2ccf484211a77fcec6aafbebd
陈孟迪 <chenmengdi@chenmengdis-MacBook-Pro.local>
2020-01-09 13:59:10 +0800

    save

---
+++ prom/deployment.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+kubectl create -f monitor-namespace.yaml
+kubectl create -f cluster-role.yaml
+kubectl create -f config-map.yaml -n monitoring
+kubectl create -f prometheus-deployment.yaml --namespace=monitoring
+kubectl create -f prometheus-service.yaml --namespace=monitoring
+kubectl get svc -n monitoring
